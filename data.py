e31ec67edc7d4e36970b7bac833bf1481d45086c
lijm1358 <lijm1358@gmail.com>
2023-04-25 12:16:47 +0000

    python black, isort

--- data.py
+++ data.py
@@ -1,54 +1,70 @@
-import cv2
-from torch.utils.data import Dataset, DataLoader
-import matplotlib.pyplot as plt
 import albumentations as A
+import cv2
+import matplotlib.pyplot as plt
 from albumentations.pytorch.transforms import ToTensorV2
+from torch.utils.data import DataLoader, Dataset
 
 
 class TrainAugmentation:
     def __init__(self, resize):
         print(resize)
-        self.transform = A.Compose([
-            A.Resize(resize[0], resize[1]),
-            A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225), max_pixel_value=255.0, always_apply=False, p=1.0),
-            ToTensorV2()
-        ])
-    
+        self.transform = A.Compose(
+            [
+                A.Resize(resize[0], resize[1]),
+                A.Normalize(
+                    mean=(0.485, 0.456, 0.406),
+                    std=(0.229, 0.224, 0.225),
+                    max_pixel_value=255.0,
+                    always_apply=False,
+                    p=1.0,
+                ),
+                ToTensorV2(),
+            ]
+        )
+
     def __call__(self, image):
         return self.transform(image=image)
-    
-    
+
+
 class TestAugmentation:
     def __init__(self, resize):
-        self.transform = A.Compose([
-            A.Resize(resize[0], resize[1]),
-            A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225), max_pixel_value=255.0, always_apply=False, p=1.0),
-            ToTensorV2()
-        ])
-    
+        self.transform = A.Compose(
+            [
+                A.Resize(resize[0], resize[1]),
+                A.Normalize(
+                    mean=(0.485, 0.456, 0.406),
+                    std=(0.229, 0.224, 0.225),
+                    max_pixel_value=255.0,
+                    always_apply=False,
+                    p=1.0,
+                ),
+                ToTensorV2(),
+            ]
+        )
+
     def __call__(self, image):
         return self.transform(image=image)
-    
+
 
 class CustomDataset(Dataset):
     def __init__(self, img_path_list, label_list, transforms=None):
         self.img_path_list = img_path_list
         self.label_list = label_list
         self.transforms = transforms
-        
+
     def __getitem__(self, index):
         img_path = self.img_path_list[index]
-        
+
         image = cv2.imread(img_path)
-        
+
         if self.transforms is not None:
-            image = self.transforms(image=image)['image']
-        
+            image = self.transforms(image=image)["image"]
+
         if self.label_list is not None:
             label = self.label_list[index]
             return image, label
         else:
             return image
-        
+
     def __len__(self):
         return len(self.img_path_list)
--- model.py
+++ model.py
@@ -1,12 +1,12 @@
+import timm
 import torch
 from torch import nn
-import timm
 
 
 class EfficientNetB0(nn.Module):
     def __init__(self):
         super().__init__()
-        self.model = timm.create_model('efficientnet_b0', pretrained=True, num_classes=19)
-    
+        self.model = timm.create_model("efficientnet_b0", pretrained=True, num_classes=19)
+
     def forward(self, x):
         return self.model(x)
